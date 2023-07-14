f0e7cf738a0ba95dde9d4ad955f494d7ee10ec93
alexwoo1900 <alexwoo20170802@gmail.com>
2023-07-14 18:34:47 +0800

    move configuration part to setting menu

--- scripts/ui/install_menu.sh
+++ scripts/ui/install_menu.sh
@@ -11,89 +11,24 @@
 
 set -e
 
-TEST_PIP_INDEX_URL="false"
-PIP_INSTALL_OPTIONS=""
-PIP_INDEX_URL=""
-
-function toggle_pip_index_url_test() {
-  if [[ $TEST_PIP_INDEX_URL == "false" ]]; then
-    TEST_PIP_INDEX_URL="true"
-  else
-    TEST_PIP_INDEX_URL="false"
-  fi
-}
-
-function set_pip_index_url() {
-  while true; do
-    read -p "Enter a new pip index url (https://pypi.org/simple): " url
-
-    if [[ $url =~ ^(http|https)://.*$ ]]; then
-      PIP_INDEX_URL=$url
-      PIP_INSTALL_OPTIONS=${PIP_INSTALL_OPTIONS}" -i ${PIP_INDEX_URL} "
-      break
-    else
-      echo "Illegal pip index url!"
-    fi
-  done
-}
-
-function process_pip_index_url() {
-  if [[ -z $PIP_INDEX_URL ]]; then
-    PIP_INDEX_URL="https://pypi.org/simple"
-  fi
-  local domain=$(echo $PIP_INDEX_URL | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_')
-  if [[ $domain =~ ^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
-    if [[ $TEST_PIP_INDEX_URL == "true" ]]; then
-      local delay=$(ping -c 4 -W 1 $domain | awk -F '/' 'END{if ($5 == "") {print "\033[31mUnreachable\033[37m"} else {print "\033[32m"$5"ms\033[37m"}}')
-      echo "$PIP_INDEX_URL $delay"
-    else
-      echo "$PIP_INDEX_URL"
-    fi
-    
-  else
-    echo "Illegal pip index url"
-  fi
-}
-
 function install_ui() {
   top_border
-  echo -e "|   ${green}~~~~~~~~~ [ Installation Configuration ] ~~~~~~~~~${white}  |"
-  hr
-  echo -e "|                                                       |"
-  echo -e "| Offline root:                                         |"
-  printf "|     %-50s|\n" "${OFFLINE_DIR}"
-  echo -e "|  1) [Reset]                                           |"
-  echo -e "|                                                       |"
-  hr
-  echo -e "|                                                       |"
-  echo -e "| pip index:                                            |"
-  printf "|     %-50s|\n" "$([[ -f "/etc/pip.conf" ]] && printf $(grep -E "^extra-index-url=" "/etc/pip.conf" | sed "s/extra-index-url=//")" (from pip.conf)" || printf "empty")"
-  if [[ $TEST_PIP_INDEX_URL == "false" ]]; then
-    printf "|     %-50s|\n" "$(process_pip_index_url)"
-    printf "|  %-53s|\n" "2) [Test On]"
-  else
-    printf "|     %-60s|\n" "$(process_pip_index_url)"
-    printf "|  %-53s|\n" "2) [Test Off]" 
-  fi
-  echo -e "|  3) [Reset]                                           |"
-  echo -e "|                                                       |"
-  hr
   echo -e "|     ${green}~~~~~~~~~~ [ Available packages ] ~~~~~~~~~~~${white}     |"
   hr
   echo -e "|                          |                            |"
   echo -e "| Firmware & API:          | 3rd Party Webinterface:    |"
-  echo -e "|  4) [Klipper]            |  9) [OctoPrint]            |"
-  echo -e "|  5) [Moonraker]          |                            |"
+  echo -e "|  1) [Klipper]            |  6) [OctoPrint]            |"
+  echo -e "|  2) [Moonraker]          |                            |"
   echo -e "|                          | Other:                     |"
-  echo -e "| Klipper Webinterface:    | 10) [PrettyGCode]          |"
-  echo -e "|  6) [Mainsail]           | 11) [Telegram Bot]         |"
-  echo -e "|  7) [Fluidd]             | 12) $(obico_install_title) |"
-  echo -e "|                          | 13) [OctoEverywhere]       |"
-  echo -e "| Touchscreen GUI:         | 14) [Mobileraker]          |"
-  echo -e "|  8) [KlipperScreen]      |                            |"
+  echo -e "| Klipper Webinterface:    |  7) [PrettyGCode]          |"
+  echo -e "|  3) [Mainsail]           |  8) [Telegram Bot]         |"
+  echo -e "|  4) [Fluidd]             |  9) $(obico_install_title) |"
+  echo -e "|                          | 10) [OctoEverywhere]       |"
+  echo -e "| Touchscreen GUI:         | 11) [Mobileraker]          |"
+  echo -e "|  5) [KlipperScreen]      |                            |"
   echo -e "|                          | Webcam Streamer:           |"
-  echo -e "|                          | 15) [Crowsnest]            |"
-  echo -e "|                          | 16) [MJPG-Streamer]        |"
+  echo -e "|                          | 12) [Crowsnest]            |"
+  echo -e "|                          | 13) [MJPG-Streamer]        |"
   echo -e "|                          |                            |"
   back_footer
 }
@@ -121,36 +56,30 @@ function install_menu() {
     read -p "${cyan}####### Perform action:${white} " action
     case "${action}" in
       1)
-	do_action "set_offline_dir" "install_ui";;
-      2)
-	do_action "toggle_pip_index_url_test" "install_ui";;
-      3)
-	do_action "set_pip_index_url" "install_ui";;
-      4)
         do_action "start_klipper_setup" "install_ui";;
-      5)
+      2)
         do_action "moonraker_setup_dialog" "install_ui";;
-      6)
+      3)
         do_action "install_mainsail" "install_ui";;
-      7)
+      4)
         do_action "install_fluidd" "install_ui";;
-      8)
+      5)
         do_action "install_klipperscreen" "install_ui";;
-      9)
+      6)
         do_action "octoprint_setup_dialog" "install_ui";;
-      10)
+      7)
         do_action "install_pgc_for_klipper" "install_ui";;
-      11)
+      8)
         do_action "telegram_bot_setup_dialog" "install_ui";;
-      12)
+      9)
         do_action "moonraker_obico_setup_dialog" "install_ui";;
-      13)
+      10)
         do_action "octoeverywhere_setup_dialog" "install_ui";;
-      14)
+      11)
         do_action "install_mobileraker" "install_ui";;
-      15)
+      12)
         do_action "install_crowsnest" "install_ui";;
-      16)
+      13)
 	do_action "install_mjpg-streamer" "install_ui";;
       B|b)
         clear; main_menu; break;;
--- scripts/ui/settings_menu.sh
+++ scripts/ui/settings_menu.sh
@@ -11,6 +11,50 @@
 
 set -e
 
+TEST_PIP_INDEX_URL="false"
+PIP_INSTALL_OPTIONS=""
+PIP_INDEX_URL=""
+
+function toggle_pip_index_url_test() {
+  if [[ $TEST_PIP_INDEX_URL == "false" ]]; then
+    TEST_PIP_INDEX_URL="true"
+  else
+    TEST_PIP_INDEX_URL="false"
+  fi
+}
+
+function set_pip_index_url() {
+  while true; do
+    read -p "Enter a new pip index url (https://pypi.org/simple): " url
+
+    if [[ $url =~ ^(http|https)://.*$ ]]; then
+      PIP_INDEX_URL=$url
+      PIP_INSTALL_OPTIONS=${PIP_INSTALL_OPTIONS}" -i ${PIP_INDEX_URL} "
+      break
+    else
+      echo "Illegal pip index url!"
+    fi
+  done
+}
+
+function process_pip_index_url() {
+  if [[ -z $PIP_INDEX_URL ]]; then
+    PIP_INDEX_URL="https://pypi.org/simple"
+  fi
+  local domain=$(echo $PIP_INDEX_URL | sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_')
+  if [[ $domain =~ ^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
+    if [[ $TEST_PIP_INDEX_URL == "true" ]]; then
+      local delay=$(ping -c 4 -W 1 $domain | awk -F '/' 'END{if ($5 == "") {printf "\033[31mx Unreachable\033[37m"} else {printf "\033[32m~ "$5"ms\033[37m"}}')
+      echo "$PIP_INDEX_URL $delay"
+    else
+      echo "$PIP_INDEX_URL"
+    fi
+    
+  else
+    echo "Illegal pip index url"
+  fi
+}
+
 function settings_ui() {
   read_kiauh_ini "${FUNCNAME[0]}"
 
@@ -58,32 +102,67 @@ function settings_ui() {
   top_border
   echo -e "|     $(title_msg "~~~~~~~~~~~~ [ KIAUH Settings ] ~~~~~~~~~~~~~")     |"
   hr
+  echo -e "|                                                       |"
+  echo -e "| Offline:                                              |"
+  echo -e "|   ● root:                                             |"
+  printf "|     %-60s|\n" "${cyan}${OFFLINE_DIR}${white}"
+  echo -e "|     1) [Reset]                                        |"
+  echo -e "|                                                       |"
+  hr
+  echo -e "|                                                       |"
+  echo -e "| pip:                                                  |"
+  echo -e "|   ● index:                                            |"
+  printf "|     %-50s|\n" "$([[ -f "/etc/pip.conf" ]] && printf $(grep -E "^extra-index-url=" "/etc/pip.conf" | sed "s/extra-index-url=//")" (from pip.conf)" || printf "empty")"
+  if [[ $TEST_PIP_INDEX_URL == "false" ]]; then
+    printf "|     %-60s|\n" "${cyan}$(process_pip_index_url)${white}"
+    printf "|     %-50s|\n" "2) [Test On]"
+  else
+    printf "|     %-70s|\n" "${cyan}$(process_pip_index_url)${white}"
+    printf "|     %-50s|\n" "2) [Test Off]" 
+  fi
+  echo -e "|     3) [Reset]                                        |"
+  echo -e "|                                                       |"
+  hr
+  echo -e "|                                                       |"
   echo -e "| Klipper:                                              |"
   echo -e "|   ● Repository:                                       |"
   printf  "|     %-70s|\n" "${custom_repo} (${custom_branch})"
+  echo -e "|     4) [Reset]                                        |"
+  echo -e "|                                                       |"
   hr
-  echo -e "| Install unstable releases:                            |"
-  printf  "|     Mainsail: %-29sFluidd: %-27s|\n" "${ms_pre_rls}" "${fl_pre_rls}"
+  echo -e "|                                                       |"
+  echo -e "| Fluidd:                                               |"
+  echo -e "|   ● release:                                          |"
+  if [[ ${fl_pre_rls} == "false" ]]; then
+    printf  "|     %-70s|\n" "Disallow unstable version"
+    echo -e "|     5) [Allow]                                        |"
+  else
+    printf  "|     %-70s|\n" "Allow unstable version"
+    echo -e "|     5) [Disallow]                                     |"
+  fi
+  echo -e "|                                                       |"
   hr
+  echo -e "|                                                       |"
+  echo -e "| Mainsail:                                             |"
+  echo -e "|   ● release:                                          |"
+  if [[ ${ms_pre_rls} == "false" ]]; then
+    printf  "|     %-70s|\n" "Disallow unstable version"
+    echo -e "|     6) [Allow]                                        |"
+  else
+    printf  "|     %-70s|\n" "Allow unstable version"
+    echo -e "|     6) [Disallow]                                     |"
+  fi
+  echo -e "|                                                       |"
+  hr
+  echo -e "|                                                       |"
   printf  "| Backup before updating: %-42s|\n" "${bbu}"
+  echo -e "|                                                       |"
   hr
-  echo -e "| 1) Set custom Klipper repository                      |"
-  blank_line
-  if [[ ${mainsail_install_unstable} == "false" ]]; then
-  echo -e "| 2) ${green}Allow${white} unstable Mainsail releases                   |"
-  else
-  echo -e "| 2) ${red}Disallow${white} unstable Mainsail releases                |"
-  fi
-  if [[ ${fluidd_install_unstable} == "false" ]]; then
-  echo -e "| 3) ${green}Allow${white} unstable Fluidd releases                     |"
-  else
-  echo -e "| 3) ${red}Disallow${white} unstable Fluidd releases                  |"
-  fi
   blank_line
   if [[ ${backup_before_update} == "false" ]]; then
-  echo -e "| 4) ${green}Enable${white} automatic backups before updates            |"
+  echo -e "| 7) ${green}Enable${white} automatic backups before updates            |"
   else
-  echo -e "| 4) ${red}Disable${white} automatic backups before updates           |"
+  echo -e "| 7) ${red}Disable${white} automatic backups before updates           |"
   fi
   back_help_footer
 }
@@ -140,15 +219,27 @@ function settings_menu() {
     case "${action}" in
       1)
         clear && print_header
-        change_klipper_repo_menu
+	      set_offline_dir
         settings_ui;;
       2)
+        clear && print_header
+	      toggle_pip_index_url_test
+        settings_ui;;
+      3)
+        clear && print_header
+	      set_pip_index_url
+        settings_ui;;
+      4)
+        clear && print_header
+        change_klipper_repo_menu
+        settings_ui;;
+      5)
         switch_mainsail_releasetype
         settings_menu;;
-      3)
+      6)
         switch_fluidd_releasetype
         settings_menu;;
-      4)
+      7)
         toggle_backup_before_update
         settings_menu;;
       B|b)
